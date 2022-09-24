da0bae309168eb266cdad0eea332f06883661a70
VictoriaT87 <victoriaemt@gmail.com>
2022-09-24 12:02:38 +0000

    add structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incremenetScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
